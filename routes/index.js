79b992278cf4d898f8211b4bc321b09806dd4273
AlexRamirezCalonge <alex.ramirez.calonge@gmail.com>
2015-05-25 16:41:06 +0200

    Subir imagenes

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,53 @@
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
 var userController = require('../controllers/user_controller');
 
-//Autoload de comandos con :quizId
-router.param('quizId', quizController.load); //autoload :quizId
-router.param('commentId', commentController.load); //autoload :commentId
-router.param('userId', userController.load); //autoload :userId
-
-/* GET home page. */
-router.get('/',sessionController.timeout, function(req, res) {
-  res.render('index', { title: 'Quiz', errors: [] });
+// Página de entrada (home page)
+router.get('/', sessionController.timeout, function(req, res) {
+res.render('index', { title: 'Quiz', errors: []});
 });
 
-//Definicion de rutas de sesion
-router.get('/login',            sessionController.new);         //formulario login
-router.post('/login',           sessionController.create);      //crear sesion
-router.get('/logout',           sessionController.destroy);     //destruir sesion
+// Autoload de comandos con ids
+router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
+router.param('userId', userController.load); // autoload :userId
 
-//Definicion de rutas de cuenta
-router.get('/user', userController.new);     //formulario sign in
-router.post('/user', userController.create);  //registrar usuario
-router.get('/user/:userId(\\d+)/edit',  sessionController.loginRequired, userController.ownershipRequired, userController.edit);
-router.put('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.update);
-router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.destroy);
+// Definición de rutas de sesion
+router.get('/login', sessionController.new); // formulario login
+router.post('/login', sessionController.create); // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
-//Definicion de rutas de /quizes
-router.get('/quizes',                      quizController.index,    sessionController.timeout);
-router.get('/quizes/:quizId(\\d+)',        quizController.show,    sessionController.timeout);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer,    sessionController.timeout);
-router.get('/quizes/new',                  sessionController.loginRequired, quizController.new,    sessionController.timeout);
-router.post('/quizes/create',              sessionController.loginRequired, quizController.create,    sessionController.timeout);
-router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit, userController.ownershipRequired,     sessionController.timeout);
-router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update, userController.ownershipRequired,    sessionController.timeout);
-router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy, userController.ownershipRequired,    sessionController.timeout);
+// Definición de rutas de cuenta
+router.get('/user', userController.new); // formulario sign un
+router.post('/user', userController.create); // registrar usuario
+router.get('/user/:userId(\\d+)/edit', sessionController.loginRequired, userController.ownershipRequired, userController.edit); // editar información de cuenta
+router.put('/user/:userId(\\d+)', sessionController.loginRequired, sessionController.timeout, userController.ownershipRequired, userController.update); // actualizar información de cuenta
+router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.destroy); // borrar cuenta
+router.get('/user/:userId(\\d+)/quizes', sessionController.timeout, quizController.index); // ver las preguntas de un usuario
 
-//Definicion de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new',   commentController.new,    sessionController.timeout);
-router.post('/quizes/:quizId(\\d+)/comments',      commentController.create,    sessionController.timeout);
-router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',     sessionController.loginRequired,  commentController.publish, userController.ownershipRequired,     sessionController.timeout);
- 
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, multer({ dest: './public/media/'}), quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, multer({ dest: './public/media/'}), quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
+
+// Definición de rutas de comentarios
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+sessionController.loginRequired, commentController.ownershipRequired, commentController.publish);
+
+//Definición de rutas adicionales
 router.get('/author', quizController.author,    sessionController.timeout);
-
 router.get('/quizes/statistics', quizController.statistics,  commentController.statistics,    sessionController.timeout);
 
-module.exports = router;
+module.exports=router;
